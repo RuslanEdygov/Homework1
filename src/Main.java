cd37ae3d158ad6c0c87ba5229448cb12a703a43a
Admin <ruslanedygov@gmail.com>
2025-02-20 21:22:03 +0300

    Cделано ДЗ условный оператор. Урок 2 №2

--- src/Main.java
+++ src/Main.java
@@ -6,31 +6,26 @@ public class Main {
         int clientOS = 1;
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        }
-        if (clientOS == 1) {
+        } else {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
         System.out.println("Задание 2");
-        int clientOS1 = 0;
-        int releaseYear = 2014;
-        if (clientOS1 == 0) {
-            if (releaseYear < 2015) {
+        int clientDeviceOS1 = 0;
+        int clientDeviceYear = 2014;
+        if ((clientDeviceOS1 == 0) || (clientDeviceYear < 2015)) {
                 System.out.println("Установите облегченную версию приложения для iOS по ссылке");
             } else {
                 System.out.println("Установите версию приложения для iOS по ссылке");
             }
-        } else if (clientOS1 == 1) {
-            if (releaseYear < 2015) {
+        if ((clientDeviceOS1 == 1) || (clientDeviceYear < 2015)) {
                 System.out.println("Установите облегченную версию приложения для Android по ссылке");
             } else {
                 System.out.println("Установите версию приложения для Android по ссылке");
             }
-        }
         System.out.println("Задание 3");
         int year = 2021;
-        if (year % 4 == 0) {
-            if ((year % 100 == 0) || (year % 400 == 0))
-                System.out.println(year + "указанный год високосный");
+        if (((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))) {
+                System.out.println(year + " указанный год високосный");
         } else {
             System.out.println(year + " указанный год не високосный");
         }
