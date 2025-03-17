c52439a449e6d19e99ad7932b189d9906f45177b
Admin <ruslanedygov@gmail.com>
2025-03-17 21:11:15 +0300

    Cделано ДЗ методы

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,51 @@ import java.util.Locale;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1");
-        String firstName = "Ivan";
-        String middleName = "Ivanovich";
-        String lastName = "Ivanov";
-        String fullName = lastName + ' ' + firstName + ' ' + middleName;
-        System.out.println("Ф. И. О. сотрудника — " + fullName);
+        int year = 2021;
+        verificationYear(year);
         System.out.println("Задание 2");
-        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " +
-                fullName.toUpperCase(Locale.forLanguageTag(fullName)));
+        int clientOS = 1;
+        int clientDeviceYear = 2016;
+        downloadApp(clientOS, clientDeviceYear);
         System.out.println("Задание 3");
-        String fullName1 = "Иванов Семён Семёнович";
-        String s = fullName1.replace('ё', 'е');
-        System.out.println(s);
+        int deliveryDistance = 95;
+        int deliveryDays = 1;
+        homeDeliveryCards(deliveryDistance, deliveryDays);
+    }
+
+    public static void verificationYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " указанный год високосный");
+        } else {
+            System.out.println(year + " указанный год не високосный");
+        }
+    }
+
+    public static void downloadApp(int clientOS, int clientDeviceYear) {
+        if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+
+    public static void homeDeliveryCards(int deliveryDistance, int deliveryDays) {
+        if (deliveryDistance > 20) {
+            deliveryDays++;
+        }
+        if (deliveryDistance > 60) {
+            deliveryDays++;
+        }
+        if (deliveryDistance > 100) {
+            System.out.println("Доставки нет");
+        } else {
+            System.out.println("Потребуется дней: " + deliveryDays);
+        }
     }
 }
+
 
